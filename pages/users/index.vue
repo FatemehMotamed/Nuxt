59a3a80f5509ea9dba421d066cca2b53600d161d
fatemeh motamed <motamed.258@gmail.com>
2020-10-10 11:40:20 +0330

    'add_user_input'

---
+++ pages/users/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" v-model="userId">
+    <input type="button" value="send" v-on:click="onLoadUser()" >
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+    data() {
+        return {
+            userId: '',
+        }
+    },
+
+    methods: {
+        onLoadUser() {
+            this.$router.push("/users/" + this.userId)
+        },
+
+
+    }
+}
+</script>
